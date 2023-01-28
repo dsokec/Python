2d11d72bf29ac29ab614c3f0245f6e9d700ad23b
Dino Šokec <dino.sokec97@gmail.com>
2023-01-28 16:48:19 +0100

    Day2 Task
    Tast
    f-String
    Python
    Like $ sign in programming language C#

---
+++ Day2/f-String-W_and_W-O.py
@@ -0,0 +1,8 @@
+#f-string
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"Tvoj rezultat je {score}, vasa visina je {height} i tim pobjedjuje {isWinning}")
+
+#print("Vas rezultat je ", str(score), ":", str(height), "-", str(isWinning))
