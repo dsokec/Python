cb8ea514ca1a969ae445514e2412441ea3666f51
Dino Šokec <dino.sokec97@gmail.com>
2023-02-01 14:05:13 +0100

    Create High Score - Coding Exercise - bez funkcija Max i Min.py
    High score
    bez funkcija Max i Min

---
+++ Day5/High Score/High Score - Coding Exercise - bez funkcija Max i Min.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+# bez upotrebe funkcija Max i Min
+
+maxScore = student_scores[0]
+
+# minScore = student_scores[0]
+for score in student_scores:
+    if maxScore < score:
+        maxScore = score
+    # Grana za Minimum Score
+    # if minScore > score:
+        # minScore = score
+
+print(f"The highest score in the class is: {maxScore}")
+
+# Min Score
+# print(f"The lowest score in the class is: {minScore}")
+
+
+
