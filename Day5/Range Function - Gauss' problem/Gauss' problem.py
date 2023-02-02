6cd7ed21b002852743428a2dba04ad4f5d119e1d
Dino Šokec <dino.sokec97@gmail.com>
2023-02-02 09:26:37 +0100

    Create Gauss' problem.py
    Sum of numbers from 1 to 100

---
+++ Day5/Range Function - Gauss' problem/Gauss' problem.py
@@ -0,0 +1,12 @@
+# Range function
+
+total = 0
+# Range from 1 to 100
+# GAUSSOV PROBLEM U OŠ
+for number in range(1, 101):
+  total += number
+print(total)
+
+# Ako zelis ispis brojeva od 1 do 10
+#for number in range(1, 11, 3):
+  #print(number)
