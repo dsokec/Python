405ac4c6bfb88845bd79fcec26ad6b3624a48fdc
Dino Šokec <dino.sokec97@gmail.com>
2023-02-02 11:06:56 +0100

    Create First Function.py
    def

---
+++ Day6/Function/First Function.py
@@ -0,0 +1,14 @@
+# Function
+# def my_function():
+  # Do this
+  # Then do this
+  # Finally do this
+
+# Osobna funkcija
+def myFunction():
+  print("Hello")
+  print("Bye!")
+
+# Calling function
+# bez ovoga funkcija nece raditi kako treba
+myFunction()
