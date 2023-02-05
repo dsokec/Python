ff930c40d85fcdf7e3d2075e74d3cfa8c13106cf
Dino Šokec <dino.sokec97@gmail.com>
2023-02-05 21:14:46 +0100

    Functions
    Function with and without parameters

---
+++ Day8/Function with Inputs/greet with parametar.py
@@ -0,0 +1,12 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+
+# Function that allows input
+def greetWithName(name):
+  print(f"Hello {name}")
+  print(f"\nHow do you do {name} ?")
+
+greetWithName("Dino")
