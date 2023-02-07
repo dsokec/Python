11b3595f643e2d229ef6704d346ea4d554ec838a
Dino Šokec <dino.sokec97@gmail.com>
2023-02-07 09:28:53 +0100

    Create Nesting Lists and Dictionaries.py
    Nesting

---
+++ Day9/Nesting List and Dictionaries/Nesting Lists and Dictionaries.py
@@ -0,0 +1,32 @@
+# Nesting 
+capitals = {
+  "France" : "Paris",
+  "Germany" : "Berlin",
+  "Spain" : "Madrid",
+}
+
+# Nesting a List in a Dictionray
+travel_log = {
+  "France" : ["Paris", "Lille", "Dijon"],
+  "Germany" : ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+# Nesting a Dictionary in a Dictionary
+travel_log = {
+  "France" : {"cities_visited" : ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany" : {"cities_visited" : ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+}
+
+# Nesting a Dictionary inside the List
+travel_log = [
+  {
+   "country": "France",
+   "cities_visited" : ["Paris", "Lille", "Dijon"],
+   "total_visits": 12
+  },
+  {
+   "country": "Germany", 
+   "cities_visited" : ["Berlin", "Hamburg", "Stuttgart"],
+   "total_visits": 5
+  },
+]
