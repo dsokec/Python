e02c0b5259304d80d25cd11ba521ce1a975e80a8
Dino Šokec <dino.sokec97@gmail.com>
2023-02-07 08:54:22 +0100

    Create The Python Dictionary Deep Dive.py
    Deep Dive
    Introduction

---
+++ Day9/Deep Dive in Python Dictionary/The Python Dictionary Deep Dive.py
@@ -0,0 +1,30 @@
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.",
+  "Function": "A piece of code that you can easily call over and over again."
+  #"Loop": "The action of doing something over and over again."
+}
+
+# Retrieving items from dictionary
+#print(programming_dictionary["Loop"])
+
+# Adding new items to dictionary
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary)
+
+# Empty dictionary
+#empty_Dictionary = {}
+
+# Wipe an existing dictionary
+#programming_dictionary = {}
+#print(programming_dictionary)
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+# Loop through your dictionary
+for key in programming_dictionary:
+  print(key)
+  # value
+  print(programming_dictionary[key])
