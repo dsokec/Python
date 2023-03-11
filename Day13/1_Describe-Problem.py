3fc5669c82fd54a343cd7b09be35c73f76f76fee
Dino Šokec <dino.sokec97@gmail.com>
2023-03-11 18:25:47 +0100

    Create 1_Describe-Problem.py
    for i in range(1,20)
    rang ide od 1 do 19

---
+++ Day13/1_Describe-Problem.py
@@ -0,0 +1,9 @@
+############DEBUGGING#####################
+
+# Describe Problem
+def my_function():
+  # for i in range(1, 20): ide od 1 do 19
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
