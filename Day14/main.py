b7e9ea96a5ed3258eb715bf9f893a150ac02d881
Dino Šokec <dino.sokec97@gmail.com>
2023-03-15 19:29:27 +0100

    Update main.py
    update
    Tasks inside comments

--- Day14/main.py
+++ Day14/main.py
@@ -2,4 +2,24 @@
 
 # 2. Generate a random account name from the list
 
+# 3. Format the account data into printable format
+
+# 4. Ask user for a guess
+
+# 5. Check if user is correct
+## 5.1. Get follower count of each account
+## 5.2. Use if statement to check if user is correct
+
+# 6. Give user feedbac on their guess
+
+# 7. Score keeping.abs
+
+# 8. Make the game repeatable.
+
+# 9. Making account at position B become the next account at position A
+
+# 10. Clear the screen between rounds.
+
+
+
 
